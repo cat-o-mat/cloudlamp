8247091674406070137cdea19dcc05352b3fe87f
cat-o-mat <bstefanowa@gmail.com>
2021-02-03 18:55:33 +0100

    2 new modes

--- CloudLamp.ino
+++ CloudLamp.ino
@@ -1,16 +1,18 @@
 #include <Adafruit_NeoPixel.h>
-#define LED_OUT 6  // LED Output
-#define D_IN 7  // Mic Digital Input (sound sensor)
-#define MC_NUMBER 50  // Number of WS2811 microcontrollers on LED strip 
-#define BUTTON 2  // Button to switch between modes
-#define MIN_LED_DURATION 90 // Amount of time the LEDs light up (in ms)
-#define MAX_LED_DURATION 150  // Amount of time the LEDs light up (in ms)
-#define MIN_LED 3 // Min number of LEDs per flash
-#define MAX_LED 5 // Max number of LEDs per flash
-#define MIN_LED_DELAY 50  // Min time between LED in a single flash (in ms)
-#define MAX_LED_DELAY 200 // Max time between LED in a single flash (in ms)
-#define MIN_FLASH_DELAY 1000  // Min time between flashes (in ms)
-#define MAX_FLASH_DELAY 2000  // Max time between flashes (in ms)
+#define LED_OUT 6              // LED Output
+#define D_IN 7                 // Mic Digital Input (sound sensor)
+#define MC_NUMBER 50           // Number of WS2811 microcontrollers on LED strip
+#define BUTTON 2               // Button to switch between modes
+#define MIN_LED_DURATION 90    // Amount of time the LEDs light up (in ms)
+#define MAX_LED_DURATION 150   // Amount of time the LEDs light up (in ms)
+#define MIN_LED 3              // Min number of LEDs per flash
+#define MAX_LED 5              // Max number of LEDs per flash
+#define MIN_LED_DELAY 50       // Min time between LED in a single flash (in ms)
+#define MAX_LED_DELAY 200      // Max time between LED in a single flash (in ms)
+#define MIN_FLASH_DELAY 1000   // Min time between flashes (in ms)
+#define MAX_FLASH_DELAY 2000   // Max time between flashes (in ms)
+#define MIN_FLASH_LINE_SIZE  1 // Min length of flash line (number of WS2800 to be turned on)
+#define MAX_FLASH_LINE_SIZE  10 // Min length of flash line (number of WS2800 to be turned on)
 #define ARRSIZE(x) (sizeof(x) / sizeof(x[0]))
 
 // Parameter 1 = number of pixels in strip
@@ -21,27 +23,26 @@
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 //   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(MC_NUMBER, LED_OUT, NEO_KHZ800 + NEO_GRB);
+Adafruit_NeoPixel strip =
+    Adafruit_NeoPixel(MC_NUMBER, LED_OUT, NEO_KHZ800 + NEO_GRB);
 
 uint32_t white = strip.Color(255, 255, 255);
 
-void setup()
-{
+void setup() {
 
-  strip.begin();  // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show(); // Turn OFF all pixels
+  strip.begin();            // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();             // Turn OFF all pixels
   strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
 
-  pinMode(D_IN, INPUT); // Set up input pin for the mic
+  pinMode(D_IN, INPUT);   // Set up input pin for the mic
   pinMode(BUTTON, INPUT); // Set up input pin for the mic
 
   Serial.begin(9600); // Serial output with 9600 bps
 }
 
-int randInRange(int min, int max)
-{
+int randInRange(int min, int max) {
   int range = 1 + (max - min);
-  int limit = range *(RAND_MAX / range);
+  int limit = range * (RAND_MAX / range);
 
   do {
     int r = rand();
@@ -50,15 +51,14 @@ int randInRange(int min, int max)
   } while (true);
 }
 
-void flashOnSound(bool initial)
-{
-  if (digitalRead(D_IN))
-  {
+void flashOnSound(bool initial) {
+  strip.clear();
+  strip.show();
+  if (digitalRead(D_IN)) {
     int leds = randInRange(MIN_LED, MAX_LED);
     int flash_delay = randInRange(MIN_FLASH_DELAY, MAX_FLASH_DELAY);
 
-    for (int i = 0; i < leds; i++)
-    {
+    for (int i = 0; i < leds; i++) {
       // Get index of random LED on the strip
       int led_idx = randInRange(0, MC_NUMBER - 1);
       // Light up random LED
@@ -69,10 +69,9 @@ void flashOnSound(bool initial)
       int light_on = randInRange(MIN_LED_DURATION, MAX_LED_DURATION);
       delay(light_on);
 
-      // Keep LED on every second round
+      // Keep LED on (almost) every second round
       int randomNum = rand();
-      if ((randomNum % 2) == 0)
-      {
+      if ((randomNum % 2) == 0) {
         // All off after one led
         strip.clear();
         strip.show();
@@ -91,22 +90,87 @@ void flashOnSound(bool initial)
   }
 }
 
-void showPattern1(bool initial)
-{
-  if (initial)
-  {
-    Serial.println("ptn1");
+void flashLineOnSound(bool initial) {
+  strip.clear();
+  strip.show();
+  if (digitalRead(D_IN)) {
+    int leds = randInRange(MIN_LED, MAX_LED);
+    int flash_delay = randInRange(MIN_FLASH_DELAY, MAX_FLASH_DELAY);
+
+    for (int i = 0; i < leds; i++) {
+      int lengthOfFlashLine = randInRange(MIN_FLASH_LINE_SIZE, MAX_FLASH_LINE_SIZE);
+      // Get index of random LED on the strip
+      int led_idx = randInRange(0, MC_NUMBER - 1);
+      // Light up random LED line
+      for (int idx = led_idx; idx < led_idx + lengthOfFlashLine; idx++) {
+        strip.setPixelColor(idx, white);   
+        strip.show(); 
+      }
+
+      int light_on = randInRange(MIN_LED_DURATION, MAX_LED_DURATION);
+      delay(light_on);
+
+      // Keep LED on (almost) every second round
+      int randomNum = rand();
+      if ((randomNum % 2) == 0) {
+        // All off after one led
+        strip.clear();
+        strip.show();
+      }
+
+      // Random delay between flashes
+      int led_delay = randInRange(MIN_LED_DELAY, MAX_LED_DELAY);
+      delay(led_delay);
+    }
+
+    // All off after a flash
     strip.clear();
-    strip.setPixelColor(1, strip.Color(255, 0, 0));
-    strip.setPixelColor(2 + 1, strip.Color(255, 0, 0));
     strip.show();
+
+    delay(flash_delay);
   }
 }
 
-void showPattern2(bool initial)
-{
-  if (initial)
-  {
+void allWhite(bool initial) {
+  if (initial) {
+    for (int i = 0; i < MC_NUMBER; i++) {
+      strip.setPixelColor(i, white);
+      strip.show();
+    }
+  }
+}
+
+// From NeoPixels Lib
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// From NeoPixels Lib
+void rainbowCycle(int wait) {
+  int i, j;
+
+  for (j = 0; j < 256 * 5; j++) { // 5 cycles of all colors on wheel
+    for (i = 0; i < strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+void showPattern2(bool initial) {
+  if (initial) {
     Serial.println("ptn2");
     strip.clear();
     strip.setPixelColor(1, strip.Color(0, 255, 0));
@@ -115,10 +179,8 @@ void showPattern2(bool initial)
   }
 }
 
-void showPattern3(bool initial)
-{
-  if (initial)
-  {
+void showPattern3(bool initial) {
+  if (initial) {
     Serial.println("ptn3");
     strip.clear();
     strip.setPixelColor(1, strip.Color(0, 0, 255));
@@ -127,28 +189,24 @@ void showPattern3(bool initial)
   }
 }
 
-void(*const PROGRAMMS[])(bool) = {&flashOnSound, &showPattern1, &showPattern2, &showPattern3};
-int mode_state = 0;  // index of light mode (starts at 0 an max=NUM_MODES)
+void (*const PROGRAMMS[])(bool) = {&flashOnSound, &allWhite, &flashLineOnSound, &showPattern2,
+                                   &showPattern3};
+int mode_state = 0; // index of light mode (starts at 0 an max=NUM_MODES)
 bool button_down = false;
 bool first_run = true;
 
-void loop()
-{
+void loop() {
   bool button_pressed = false;
 
-  if (digitalRead(BUTTON))
-  {
+  if (digitalRead(BUTTON)) {
     button_down = true;
-  }
-  else if (button_down)
-  {
+  } else if (button_down) {
     // butoon was released just now
     button_down = false;
     button_pressed = true;
   }
 
-  if (button_pressed)
-  {
+  if (button_pressed) {
     mode_state++;
   }
   PROGRAMMS[mode_state % ARRSIZE(PROGRAMMS)](button_pressed || first_run);
